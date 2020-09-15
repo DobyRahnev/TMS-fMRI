5f177723ef4f93d2b7ff848a2eba98db83e8c9a8
frafiei3 <farshad@gatech.edu>
2020-09-14 22:01:39 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TMS-fMRI
 This repository contains our TMS-fMRI paper and relevant codes
+This is the tutorial for Rahnev lab.
---
+++ intro.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import pydicom
+import png
+# import re
+import os
+from pandas import ExcelWriter
+from pandas import ExcelFile
+print(5//2)
+
+# ds = pydicom.dcmread('IM-0077-0001.dcm')
+# print(ds[0x0018, 0x1400].value)
+# print(ds)
+# shape = ds.pixel_array.shape
+# image_2d = ds.pixel_array.astype(float)
+# image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+# image_2d_scaled = np.uint8(image_2d_scaled)
+
+# destination = os.path.dirname(os.path.realpath(__file__))
+# with open(destination + '/farshad.png', 'wb') as png_file:
+#     w = png.Writer(shape[1], shape[0], greyscale=True)
+#     w.write(png_file, image_2d_scaled)
+
+
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+# print(destination + '/farshad.jpg')
+
+
+
+
+
+
+
+
+
+	
